373e78bf6e960fc78625357c11b15968c5175d20
Max Rydkin <maks.rydkin@gmail.com>
2012-11-03 18:04:24 +0400

    fix slug_validator name

--- lib/slug_validator.rb
+++ lib/slug_validator.rb
@@ -1,4 +1,4 @@
-class SlugFormatValidator < ActiveModel::EachValidator
+class SlugValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     unless value =~ /^\D(\w*-*)*$/
       object.errors[attribute] << (options[:message] || "is not formatted properly (should contain only letters, digits, dashes and underscores and can't start with digit)")
